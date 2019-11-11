36426564e939ccd10b6b6212d72e9aebef980fb3
bella-cockrell <heybellac@gmail.com>
2019-11-11 23:16:04 +0000

    First commit of clock project

---
+++ clock.js
@@ -0,0 +1,14 @@
+//Filler comment //
+
+var wakeuptime = 7;
+var noon = 12;
+var lunchtime = 12;
+var naptime = lunchtime + 2;
+var partytime;
+var evening = 18;
+
+//This bit shows the current time on the page
+var showCurrentTime = function()
+{    
+
+}
